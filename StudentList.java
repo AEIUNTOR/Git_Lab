d2eed3b332818e9f93e582602b7404e9c26d5e7d
AEIUNTOR <antorfahimshakil@gmail.com>
2023-10-11 17:01:26 +0600

    Adding more comments and making more naming improvement

--- StudentList.java
+++ StudentList.java
@@ -18,8 +18,8 @@ public class StudentList{
             String i[]=r.split(",");
             for(String j:i){
                 System.out.println(j);
-                
-            }catch (Exception e){
+                //exception handling
+            }catch (Exception e){ 
                 System.out.println("Data Loaded...");
             }
         }
@@ -37,7 +37,7 @@ public class StudentList{
                 System.out.println(r);
             String i[]=r.split(",");
             Random var=new Random();
-            int y=var.nextInt();
+            int y=var.nextInt(4); //4 should be added
             System.out.println(i[y]);
 
         }catch(Exception e){
@@ -48,6 +48,7 @@ public class StudentList{
     else if(args[0].equals("r")){
         System.out.println("Loading data...");
         try{
+            //buffer function for writing file
             BufferedWriter string= new BufferedWriter(
                 new FileWriter("students.txt",true);
             )
@@ -67,6 +68,7 @@ public class StudentList{
 else if(args[0].contains("?")){
     System.out.println("Loading data...");
     try{
+        //buffer function for reading file
         BufferedReader string= new BufferedReader(
             new InputStreamReader(
                 new FileInputStream("students.txt");
@@ -76,8 +78,8 @@ else if(args[0].contains("?")){
             String i[]=r.split(",");
             boolean done=false;
             String t=args[0].substring(1);
-            for(int idx=0;idx<i.length && !done;idx++){
-                if(i[idx].equals(t)){
+            for(int index=0;index<i.length && !done;index++){
+                if(i[index].equals(t)){
                     System.out.println("We found it!");
                     done=true;
                 }
@@ -95,12 +97,12 @@ else if(args[0].contains("c")){
                 new FileInputStream("students.txt");
                
             ))
-            String D=string.readLine();
-            char a[]=D.toCharArray();
+            String Data=string.readLine();
+            char array[]=Data.toCharArray();
             boolean in_word=false;
             int count=0;
-            for(char c:a){
-                if(c==','){
+            for(char c:array){
+                if(c==','){ // simplifies the count logic
                     if(!in_word){
                         count++;
                         in_word =true;
@@ -109,7 +111,7 @@ else if(args[0].contains("c")){
                     }
                 }
             }
-            System.out.println(count+"word(string) found "+ a.length);
+            System.out.println(count+"word(string) found ") ; // no need of  (a.length)
         }catch(Exception e){
             System.out.println("Data Loaded...");
         }
